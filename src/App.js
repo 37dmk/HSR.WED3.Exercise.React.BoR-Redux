7bf22fc6f55592b08b035e2384ea2d387fc2d02e
Mirko Stocker <mirko@stocker.email>
2020-04-19 10:51:13 +0200

    make App a function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
   Link,
-  withRouter
+  withRouter,
 } from "react-router-dom";
 import { Container, Menu, Segment } from "semantic-ui-react";
 
@@ -17,82 +17,79 @@ import PrivateRoute from "./components/PrivateRoute";
 import { connect } from "react-redux";
 import { authenticate, signout } from "./actions";
 
-class App extends React.Component {
-  render() {
-    const { isAuthenticated, user, token } = this.props;
-    const MenuBar = withRouter(({ history, location: { pathname } }) => {
-      if (isAuthenticated && user) {
-        return (
-          <Segment.Group style={{ marginTop: "1em" }}>
-            <Segment>
-              <Menu pointing secondary color="blue">
-                <Menu.Item header>
-                  {user.firstname} {user.lastname} &ndash; {user.accountNr}
-                </Menu.Item>
+function App({ isAuthenticated, user, token, ...props }) {
+  const MenuBar = withRouter(({ history, location: { pathname } }) => {
+    if (isAuthenticated && user) {
+      return (
+        <Segment.Group style={{ marginTop: "1em" }}>
+          <Segment>
+            <Menu pointing secondary color="blue">
+              <Menu.Item header>
+                {user.firstname} {user.lastname} &ndash; {user.accountNr}
+              </Menu.Item>
+              <Menu.Item
+                name="Kontoübersicht"
+                active={pathname === "/dashboard"}
+                as={Link}
+                to="/dashboard"
+              />
+              <Menu.Item
+                name="Zahlungen"
+                active={pathname === "/transactions"}
+                as={Link}
+                to="/transactions"
+              />
+              <Menu.Menu position="right">
                 <Menu.Item
-                  name="Kontoübersicht"
-                  active={pathname === "/dashboard"}
-                  as={Link}
-                  to="/dashboard"
+                  name={`Logout ${user.firstname} ${user.lastname}`}
+                  onClick={() => {
+                    props.signout(() => history.push("/"));
+                  }}
                 />
-                <Menu.Item
-                  name="Zahlungen"
-                  active={pathname === "/transactions"}
-                  as={Link}
-                  to="/transactions"
-                />
-                <Menu.Menu position="right">
-                  <Menu.Item
-                    name={`Logout ${user.firstname} ${user.lastname}`}
-                    onClick={() => {
-                      this.props.signout(() => history.push("/"));
-                    }}
-                  />
-                </Menu.Menu>
-              </Menu>
-            </Segment>
-          </Segment.Group>
-        );
-      } else {
-        return null;
-      }
-    });
+              </Menu.Menu>
+            </Menu>
+          </Segment>
+        </Segment.Group>
+      );
+    } else {
+      return null;
+    }
+  });
 
-    return (
-      <Router>
-        <Container>
-          <MenuBar />
-          <Route
-            exact
-            path="/"
-            render={props => (
-              <Home {...props} isAuthenticated={isAuthenticated} />
-            )}
-          />
-          <Route
-            path="/login"
-            render={props => (
-              <Login {...props} authenticate={this.props.authenticate} />
-            )}
-          />
-          <Route path="/signup" component={Signup} />
-          <PrivateRoute
-            isAuthenticated={isAuthenticated}
-            token={token}
-            path="/dashboard"
-            component={Dashboard}
-          />
-          <PrivateRoute
-            isAuthenticated={isAuthenticated}
-            token={token}
-            user={user}
-            path="/transactions"
-            component={Transactions}
-          />
-        </Container>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <Home {...props} isAuthenticated={isAuthenticated} />
+          )}
+        />
+        <Route
+          path="/login"
+          render={(props) => (
+            <Login {...props} authenticate={this.props.authenticate} />
+          )}
+        />
+        <Route path="/signup" component={Signup} />
+        <PrivateRoute
+          isAuthenticated={isAuthenticated}
+          token={token}
+          path="/dashboard"
+          component={Dashboard}
+        />
+        <PrivateRoute
+          isAuthenticated={isAuthenticated}
+          token={token}
+          user={user}
+          path="/transactions"
+          component={Transactions}
+        />
+      </Container>
+    </Router>
+  );
 }
 
 const mapStateToProps = (state) => {
